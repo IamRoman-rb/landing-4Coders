07c03f7443c8c5bc1f5f721c8fa18f95bbf16714
IamRoman-rb <roman.borla2004@mail.com>
2021-09-18 17:50:06 -0300

    pre-loader

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,14 @@ const links = document.querySelector("#links");
 burguerMenu.addEventListener("click", (e) => {
     links.classList.toggle("active");
 })
+
+
+//Pre-loader
+const preLoader = document.querySelector(".pre-loader");
+window.addEventListener("load", e => {
+    setTimeout(() => {
+        preLoader.style.opacity = "0";
+        preLoader.style.zIndex = "0"    
+    },
+        1000)
+});
